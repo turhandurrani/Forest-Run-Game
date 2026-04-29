6b068a9cd261e20459615fa6ad42c07bb71f44da
turhandurrani <turhandurrani@gmail.com>
2026-04-29 18:32:55 +0500

    Add Player class with input handling, state management, and physics updates

---
+++ Player.cpp
@@ -0,0 +1,80 @@
+#include "Player.hpp"
+#include <SFML/Window/Keyboard.hpp>
+using namespace sf;
+
+Player::Player(float starX, float startY): GameObject(starX, floorY - 60, 40, 60){
+    gravity = GRAVITY;
+    this->floorY = floorY - 60;
+    onGround = true;
+    isHovering = false;
+    hoverTimer = 0.0f;
+    state = PlayerState::RUNNING;
+}
+
+void Player::handleInput(){
+    if(Keyboard::isKeyPressed(Keyboard::Key::Space) && onGround){
+        velY = JUMP_FORCE;
+        onGround = false;
+        state = PlayerState::JUMPING;
+    }
+    
+    if(Keyboard::isKeyPressed(Keyboard::Key::Space) && !onGround){
+        if(hoverTimer < MAX_HOVER_TIME){
+            isHovering = true;
+            hoverTimer += 0.016f;
+            state = PlayerState::HOVERING;
+        }
+        else{
+            isHovering = false;
+        }
+    }
+    else if (onGround){
+        isHovering = false;
+        hoverTimer = 0.0f;
+    }
+
+    if(Keyboard::isKeyPressed(Keyboard::Down)){
+        PlayerState::DUCKING;
+        height = DUCK_HEIGHT;
+    }
+    else if (onGround && state != PlayerState::JUMPING){
+        height = STAND_HEIGHT;
+        if(state != PlayerState::HOVERING){
+            state = PlayerState::RUNNING;
+        }
+    }
+}
+
+void Player::update(float deltaTime){
+    float currentGravity = isHovering ? HOVER_GRAVITY : gravity;
+    velY += currentGravity * deltaTime;
+    y += velY * deltaTime;
+
+    if (y >= floorY){
+        y = floorY;
+        velY = 0;
+        onGround = true;
+        isHovering = false;
+        hoverTimer = 0.0f;
+        if(state != PlayerState::DUCKING){
+            state = PlayerState::DUCKING;
+        }
+    }
+}
+
+void Player::reset(){
+    y = 0;
+    velY = 0;
+    velX = 0;
+    gravity = GRAVITY;
+    onGround = true;
+    isHovering = false;
+    hoverTimer = 0.0f;
+    state = PlayerState::RUNNING;
+    height = STAND_HEIGHT;
+}
+
+PlayerState Player::getState() const {return state;}
+bool Player::getIsHovering() const {return isHovering;}
+bool Player::getOnGround() const {return getOnGround;}
+float Player::getHoverTime() const {return hoverTimer;}
